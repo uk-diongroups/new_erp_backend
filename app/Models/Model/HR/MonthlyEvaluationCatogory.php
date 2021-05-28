0a3e62fdade28f62cedb94ec8543214fabbb2772
mac <suleimamman@gmail.com>
2021-05-28 08:56:18 +0100

    Monthly Evaluation test file created

---
+++ app/Models/Model/HR/MonthlyEvaluationCatogory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Model\HR;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MonthlyEvaluationCatogory extends Model
+{
+    use HasFactory;
+}
